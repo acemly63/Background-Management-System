0f95333e3b8dcf64406be2606e1191cb872a40b2
acemly63 <acemly63@163.com>
2020-06-18 21:31:31 +0800

    完成了分类功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, card, row, col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, tree, Select, Option } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, card, row, col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, tree, Select, Option, Cascader } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -27,5 +27,6 @@ Vue.use(Tag)
 Vue.use(tree)
 Vue.use(Select)
 Vue.use(Option)
+Vue.use(Cascader)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
